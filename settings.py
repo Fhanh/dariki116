033b5848c355ef13c80cd039a005c00a1c38eb2f
Ranis <nervice@bk.ru>
2022-05-15 15:36:08 +0300

    Deleted

--- settings.py
+++ settings.py
@@ -2,11 +2,11 @@ import logging
 import datetime
 
 # general
-TG_TOKEN = '5163513055:AAGJxaU2na1kEcJTcEGdm3Cxs8ApH1pjRNQ'
+TG_TOKEN =
 
 logging.basicConfig(filename="logs.txt", level=logging.INFO)
 logger = logging.getLogger(__name__)
 time = datetime.datetime.now()
 
 # admin access
-ADMINS_ID = [881375721, 1635977782, 1050164391]
+ADMINS_ID =
